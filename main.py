d809bf9083626b08d594480140989080ec4718d0
ChronoX27 <lu.hauber@web.de>
2024-04-22 10:37:55 +0200

    weiterarbeit, bis her nicht so nice

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from time import sleep
-from picarx import Picarxööö  
+from picarx import Picarx
+
 # from vilib import Vilib
-from robot_hat import Music, TTS
+# from robot_hat import Music, TTS
+
+px = Picarx()
+POWER = 50  # Geschwindigkeit des Robos beim Fahren.
+LENKUNG = 30  # Einschlag des Lenkmotors
+SAFE = 40  # Sagt dem Robo wie viel Sicherheitsabstand zu Objekten genug ist.
+DANGER = 20  # Sagt dem Robo ab wie viel Abstand er sich sorgen machen muss. (Lenken)
+
+# POWER = 50
+# SafeDistance = 40   # > 40 safe
+# DangerDistance = 20 # > 20 && < 40 turn around,
+#                     # < 20 backward
 
 
 def reset():
@@ -11,81 +23,65 @@ def reset():
     px.set_dir_servo_angle(0)
 
 
-POWER = 10  # Geschwindigkeit des Robos beim Fahren.
-LENKUNG = 35  # Einschlag des Lenkmotors
-SAFE = 45  # Sagt dem Robo wie viel Sicherheitsabstand zu Objekten genug ist.
-DANGER = 30  # Sagt dem Robo ab wie viel Abstand er sich sorgen machen muss. (Lenken)
-
-
-def vor():
-    px.forward(POWER)
-    return "vor"
-
-
-def zurück():
-    px.backward(POWER)
-    return "back"
-
-
-def links():
-    return "links"
-
-
-def rechts():
-    return "rechts"
-
-
 def main():
     """Hauptschleife für den Roboter"""
     reset()
 
-    try:
-        last = ""
-        while True:
-            distance = round(px.ultrasonic.read())
-            # Gerade aus fahren wenn sicht frei ist.
-            if distance >= SAFE:
-                # schützt vor endlos schleife
-                if last == "back":
-                    print("zurück > vor Abstand: " + str(distance))
-                    px.set_dir_servo_angle(-LENKUNG)
-                else:
-                    px.set_dir_servo_angle(0)
-                px.forward(POWER)
-                last = "vor"
-            # Wird gefährlich, Richtung ändern. (Wand oder Hinderniss zu nah)
-            elif distance >= DANGER:
-                px.set_dir_servo_angle(-LENKUNG)
-                px.forward(POWER)
+    # last = ""
+    while True:
+        distance = round(px.ultrasonic.read())
+        # Gerade aus fahren wenn sicht frei ist.
+        if distance >= SAFE:
+            # schützt vor endlos schleife
+            # if last == "back":
+            #     print("zurück > vor Abstand: " + str(distance))
+            #     px.set_dir_servo_angle(-LENKUNG)
+            # else:
+            #     px.set_dir_servo_angle(0)
+            px.set_dir_servo_angle(0)
+            px.forward(POWER)
+        # last = "vor"
+        # Wird gefährlich, Richtung ändern. (Wand oder Hinderniss zu nah)
+        elif distance >= DANGER:
+            print("lenkmanöver")
+            # px.set_dir_servo_angle(LENKUNG)
+            # px.backward(POWER)
+            # px.set_dir_servo_angle(-LENKUNG)
+            # px.forward(POWER)
+            # sleep(0.1)
+            px.set_dir_servo_angle(LENKUNG)
+            px.forward(POWER)
+            sleep(0.1)
 
-                last = "steuern"
-            # Kein Ausweg mehr, Robo ist zu nah am Hindernis oder der Wand um zu fahren.
-            else:
-                # schützt vor endlos schleife
-                if last == "vor":
-                    print("vor > zurück Abstand: " + str(distance))
-                    px.set_dir_servo_angle(LENKUNG)
-                else:
-                    px.set_dir_servo_angle(0)
-                px.backward(POWER)
-
-                last = "back"
+            # last = "steuern"
+        # Kein Ausweg mehr, Robo ist zu nah am Hindernis oder der Wand um zu fahren.
+        else:
+            print("zurück")
+            # # schützt vor endlos schleife (vielleicht)
+            # if last == "vor":
+            #     print("vor > zurück Abstand: " + str(distance))
+            #     px.set_dir_servo_angle(LENKUNG)
+            # else:
+            #     px.set_dir_servo_angle(0)
+            px.set_dir_servo_angle(-LENKUNG)
+            px.backward(POWER)
             sleep(0.5)
-    finally:
-        px.forward(0)
-        reset()
+            # last = "back"
 
 
-px = Picarx()
 
-tts = TTS()
-tts.lang("de-DE")
-music = Music()
-music.music_set_volume(10)
 
-# für aufgabe 1 nicht benötigt
+
+# tts = TTS()
+# tts.lang("de-DE")
+# music = Music()
+# music.music_set_volume(10)
+
+# Für Aufgabe 1 nicht benötigt
 # Vilib.camera_start(vflip=False,hflip=False)
 # Vilib.display(local=True,web=True)
+
+# px = Picarx()
 
 if __name__ == "__main__":
     main()
--- sensor_test.py
+++ sensor_test.py
@@ -1,7 +1,21 @@
 from time import sleep
 from picarx import Picarx
 
+l_before, m_before, r_before = 0, 0, 0
+
 px = Picarx()
 while True:
-    print(px.ultrasonic.read())
+    print(f"Ultraschall: \t{px.ultrasonic.read()}")
+    l_now = px.grayscale.read(0)
+    m_now = px.grayscale.read(1)
+    r_now = px.grayscale.read(2)
+    print(
+        f"Links: \t{l_now} ({l_now - l_before})\t \
+        Mitte: \t{m_now} ({m_now - m_before})\t \
+        Rechts:\t{r_now} ({r_now - r_before })"
+    )
+
+    l_before = l_now
+    m_before = m_now
+    r_before = r_now
     sleep(1)
